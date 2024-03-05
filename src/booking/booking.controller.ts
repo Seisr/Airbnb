bc0e414b0d5542e50f193589de09e8a8cec664d8
Bam <khanguyen1204@gmail.com>
2024-03-06 00:18:48 +0700

    feat: add api getBookingByBookingId

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { BookingService } from './booking.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Booking')
 @Controller('booking')
@@ -10,6 +10,12 @@ export class BookingController {
   @Get('/getAllBooking')
   async getAllBooking(@Res() res): Promise<any> {
     let data = await this.bookingService.getAllBooking();
+    res.status(data.status).json(data);
+  }
+  @ApiParam({ name: 'id', required: true })
+  @Get('/getBookingByBookingId/:id')
+  async getBookingById(@Param('id') id, @Res() res): Promise<any> {
+    let data = await this.bookingService.getBookingById(+id);
     res.status(data.status).json(data);
   }
 }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -18,4 +18,22 @@ export class BookingService {
       };
     }
   }
+  async getBookingById(id: number): Promise<any> {
+    try {
+      let data = await this.prisma.dat_phong.findFirst({
+        where: {
+          id: id,
+        },
+      });
+      return {
+        status: 200,
+        data: data,
+      };
+    } catch (e) {
+      return {
+        status: 500,
+        message: `getBookingById error ${e}`,
+      };
+    }
+  }
 }
