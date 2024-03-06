1a6f8f7bc27852a1075f16815568a4fd47b0cc45
Bam <khanguyen1204@gmail.com>
2024-03-06 11:46:13 +0700

    feat: add api editBooking

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Res } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { bookingDTO } from './dto/booking.dto';
@@ -20,9 +20,20 @@ export class BookingController {
     res.status(data.status).json(data);
   }
 
-  @Post('/postBooking')
+  @Post('/postBooking/')
   async postBooking(@Body() body: bookingDTO, @Res() res): Promise<any> {
     let data = await this.bookingService.postBooking(body);
+    res.status(data.status).json(data);
+  }
+
+  @ApiParam({ name: 'id' })
+  @Put('/editBooking/:id')
+  async editBooking(
+    @Param('id') id,
+    @Body() body: bookingDTO,
+    @Res() res,
+  ): Promise<any> {
+    let data = await this.bookingService.editBooking(+id, body);
     res.status(data.status).json(data);
   }
 }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -63,4 +63,33 @@ export class BookingService {
       };
     }
   }
+  async editBooking(id: number, body: bookingDTO): Promise<any> {
+    try {
+      let { ma_phong, ngay_den, ngay_di, so_luong_khach, ma_nguoi_dat } = body;
+      let ngay_den2 = new Date(ngay_den).toISOString();
+      let ngay_di2 = new Date(ngay_di).toISOString();
+      let newBooking = {
+        ma_phong: ma_phong,
+        ngay_den: ngay_den2,
+        ngay_di: ngay_di2,
+        so_luong_khach: so_luong_khach,
+        ma_nguoi_dat: ma_nguoi_dat,
+      };
+      await this.prisma.dat_phong.update({
+        where: {
+          id: id,
+        },
+        data: newBooking,
+      });
+      return {
+        status: 200,
+        message: 'edit booking thành công',
+      };
+    } catch (e) {
+      return {
+        status: 500,
+        message: `editBooking error ${e}`,
+      };
+    }
+  }
 }
